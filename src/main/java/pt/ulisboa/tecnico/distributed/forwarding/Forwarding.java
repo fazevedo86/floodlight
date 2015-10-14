65e89474d7d7f3001477d777c7c3b2d3546e4152
Filipe Azevedo <fazevedo@netmadeira.com>
2015-10-14 17:16:21 +0100

    debug messages

--- src/main/java/pt/ulisboa/tecnico/distributed/forwarding/Forwarding.java
+++ src/main/java/pt/ulisboa/tecnico/distributed/forwarding/Forwarding.java
@@ -267,6 +267,8 @@ public class Forwarding extends ForwardingBase implements IFloodlightModule, ISy
 			}
 			
 			Forwarding.log.info("Processing new IPv4 Flow: src=" + srcIP + " dst=" + dstIP);
+		} else {
+			Forwarding.log.info("Processing new non-IPv4 (ether=" + eth.getEtherType().getValue() + ")  Flow: src=" + eth.getSourceMACAddress() + " dst=" + eth.getDestinationMACAddress());
 		}
 		NetworkHost src = new NetworkHost(eth.getSourceMACAddress().getLong(), eth.getSourceMACAddress().toString(), Short.valueOf(eth.getVlanID()), srcIP);
 		NetworkHost dst = new NetworkHost(eth.getDestinationMACAddress().getLong(), eth.getDestinationMACAddress().toString(), Short.valueOf(eth.getVlanID()), dstIP);
